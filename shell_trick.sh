ed35d685d43a0e69eeeb8d0157964494182fd337
Khang Pham <khangpham@ymail.com>
2015-03-03 14:35:54 -0800

    Update shell_trick.sh

--- shell_trick.sh
+++ shell_trick.sh
@@ -2,3 +2,6 @@
 
 # Check if a variable contains digits only
 [[ "10202a03" =~ ^-?[0-9]+$ ]] && echo "True"
+
+# Filter out error that is not important
+ 2>>>(grep -v "some error message I don't care about")
